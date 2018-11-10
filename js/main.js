b1831436dfb8507cd754ca067e1d2c8411a7bfb4
Terence Chambers <chambetc@mail.uc.edu>
2018-11-10 15:25:59 -0500

    working on lvl 1 and lvl2 arrays

--- js/main.js
+++ js/main.js
@@ -8,10 +8,23 @@ var lvl1[3] = "Tree", "", "4", "neutral";
 var lvl1[4] = "Petroleum", "", "5", "neutral";
 var lvl1[5] = "Iron", "", "6", "neutral";
 
+
+//Level2
+var lvl2 = ["name", "description", "price", "category"];
+var lvl2[0] = "River", "", "1", "good";
+var lvl2[1] = "Crop Field", "", "2", "good";
+var lvl2[2] = "Mountain", "", "3", "good";
+var lvl2[3] = "Tree", "", "4", "good";
+var lvl2[4] = "Rainforest", "", "5", "good";
+var lvl2[5] = "Gold", "", "6", "good";
+var lvl2[6] = "Ocean", "", "8", "good";
+var lvl2[7] = "Dragon", "", "7", "bad";
+var lvl2[8] = "Volcano", "", "8", "bad";
+var lvl2[9] = "Flying Pig", "", "9", "bad";
+
 function selectItem(){
 
 var selectItem = document.getElementById();
-
+for (i = 0; i < 7; i++){
+  console.log ( " good job !" )
 }
-
-//Level2
