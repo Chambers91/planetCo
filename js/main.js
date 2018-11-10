81c7adda419e359acfe3d19aee4b323a7613cb09
Terence Chambers <chambetc@mail.uc.edu>
2018-11-10 14:57:49 -0500

    created the array for level 1 items

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//create an array that you have items to select from
+// points/currency do not change based off the selection because it is level 1
+var lvl1 = ["name", "description", "price", "category"];
+var lvl1[0] = "River", "", "1", "neutral";
+var lvl1[1] = "Crop Field", "", "2", "neutral";
+var lvl1[2] = "Mountain", "", "3", "neutral";
+var lvl1[3] = "Tree", "", "4", "neutral";
+var lvl1[4] = "Petroleum", "", "5", "neutral";
+var lvl1[5] = "Iron", "", "6", "neutral";
+
+function selectItem(){
+
+var selectItem = document.getElementById();
+
+}
+
+//Level2
