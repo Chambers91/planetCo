b739a3b06c59ad97141bcb5fe07b4b26d664f0d9
Eric Anspach <eric.j.anspach@gmail.com>
2018-11-10 20:18:50 -0500

    Modified arrays for levels 1 and 2

---
+++ js/main-arrays.js
@@ -0,0 +1,53 @@
+// ARRAYS FOR LEVELS 1 AND 2
+
+//create an array that you have items to select from
+// points/currency do not change based off the selection because it is level 1
+var lvl1 = [
+      ["River", 350, "good"],
+      ["Tree", 175, "good"],
+      ["Crop Field", 200, "good"],
+      ["Mountain", 200, "good"],
+      ["Gold", 425, "good"],
+      ["Giant Butterflies", 50, "good"]
+];
+
+//Level2
+var lvl2 = [
+      ["River", 350, "good"],
+      ["Rainforest", 450, "good"],
+      ["Tree", 175, "good"],
+      ["Ocean", 400, "good"],
+      ["Mountain", 200, "good"],
+      ["Volcano", 175, "good"],
+      ["Gold", 425, "good"],
+      ["Flying Pig", 50, "bad"],
+      ["Dragon", 175, "bad"]
+];
+
+function selectItem(){
+
+var selectItem = document.getElementById();
+for (i = 0; i < 7; i++){
+  console.log ( " good job !" )
+}
+
+// Level 1
+// 
+// Screen displays six choices for user.
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
